bf24ca958adfea2f4d481adc36b64dd394b3ab19
MinexAutomation <minex.automation@gmail.com>
2020-01-22 00:31:22 -0800

    Initial commit of source-stub.

---
+++ source/R5T.Glenrothes.Findhorn.Construction/Code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace R5T.Glenrothes.Findhorn.Construction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
